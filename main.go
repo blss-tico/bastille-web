a2a7d9e6de5de9832dbbc68b5dd650585d3567cf
blss-tico <bruno.ccutp@gmail.com>
2025-08-01 08:38:46 -0300

    feat: Added LoggingMiddleware function to main file

--- main.go
+++ main.go
@@ -26,6 +26,15 @@ func init() {
 	}
 }
 
+func LoggingMiddleware(f http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		sessionId := r.Header.Get("X-Request-ID")
+		start := time.Now()
+		log.Printf("%s %s %s %v %s", r.Method, r.URL.Path, r.RemoteAddr, time.Since(start), sessionId)
+		f(w, r)
+	}
+}
+
 func main() {
 	log.Println("main")
 	mux := http.NewServeMux()
