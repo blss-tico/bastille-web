dd30b74dab290710e4e4f6cec26c2e75767a3085
blss-tico <bruno.ccutp@gmail.com>
2026-01-08 18:03:18 -0300

    feat: changed templates to add select field with sessionStorage jails.

--- static/index.js
+++ static/index.js
@@ -1,8 +1,17 @@
-window.onload = () => {  
+window.onload = () => {
+  sessionStorage.removeItem('id');
+  sessionStorage.removeItem('jails');
+
   const uuidVal = uuidv7Generate();
   const uuidStr = Array.from(uuidVal).map((b) => b.toString(16).padStart(2, "0")).join("");
   const uuidFmt = uuidv7StringFormat(uuidStr);
   sessionStorage.setItem('id', uuidFmt);
+
+  setTimeout(async () => {
+    const formJson = JSON.stringify({"action":"all","options":"-j"});
+    const jails = await sendHttpRequest("list", formJson);
+    if (jails.data.msg) { sessionStorage.setItem('jails', jails.data.msg); }
+  }, 500);
 }
 
 async function sendHttpRequest(url, bodyData) {
@@ -19,12 +28,12 @@ async function sendHttpRequest(url, bodyData) {
 
   if (!response.ok) {    
     const error = await response.text();
-    // console.log('index.js - sendHttpRequest - error - response status: ', response.status, error);
+    //console.log('index.js - sendHttpRequest - error - response status: ', response.status, error);
     return { code: response.status, error: error };
   }
   
   const responseData = await response.json();
-  // console.log('index.js - sendHttpRequest - response status: ', response.status, responseData);
+  //console.log('index.js - sendHttpRequest - response status: ', response.status, responseData);
   return { code: response.status, data: responseData };
 }
 
