90b110222186730acb3914057bb1e095fc6a1ca7
blss-tico <bruno.ccutp@gmail.com>
2026-01-14 15:26:51 -0300

    fix: Erased addrModel from loggingMidleware.

--- middlewares.go
+++ middlewares.go
@@ -4,20 +4,10 @@ import (
 	"log"
 	"net/http"
 	"time"
-
-	//"bastille-web/docs"
 )
 
 func loggingMiddleware(f http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		/*
-		if len(addrModel) == 0 {
-			addrModel = getClientIPAddrUtil(r)
-			docs.SwaggerInfo.Host = addrModel
-			log.Println("addrModel: ", addrModel)
-		}
-		*/
-
 		sessionId := r.Header.Get("X-Request-ID")
 		start := time.Now()
 		log.Printf("%s %s %s %v %s", r.Method, r.URL.Path, r.RemoteAddr, time.Since(start), sessionId)
